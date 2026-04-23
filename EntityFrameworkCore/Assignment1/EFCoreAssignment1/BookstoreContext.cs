fd27792562f31e18e283993e00a564d8f36da9f6
OmarMShawky <oshawky96@outlook.com>
2026-04-23 20:25:18 +0200

    Add files via upload

---
+++ EntityFrameworkCore/Assignment1/EFCoreAssignment1/BookstoreContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+using ReadMoreBooks.Models;
+
+namespace ReadMoreBooks.Data;
+
+public class BookstoreContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            @"Server=.;Database=ReadMoreBooksDB;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+}
